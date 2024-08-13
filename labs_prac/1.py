b35f015fa5bbe9a3faa40488227096ece6af8018
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-08-13 18:37:20 +0300

    do dododlabs

--- labs_prac/1.py
+++ labs_prac/1.py
@@ -1,1 +1,8 @@
-С=ыхыхыхвввааааыфааыыа
+import numpy as np
+
+A = np.array([[0.3, 2.5, -2.5], [2.4, 7.5, 4.1], [-1.1, 1.2, 4.9]])
+B = np.array([0.1, 7.3, 5.5])
+
+solution = np.linalg.solve(A, B)
+
+print("Решение системы уравнений:", solution)
