3dc3e721ffb0e763c11e4e9e9278a46bc4c337d2
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-08-13 18:43:02 +0300

    do

--- labs_prac/1.py
+++ labs_prac/1.py
@@ -3,6 +3,14 @@ import numpy as np
 A = np.array([[0.3, 2.5, -2.5], [2.4, 7.5, 4.1], [-1.1, 1.2, 4.9]])
 B = np.array([0.1, 7.3, 5.5])
 
+np.savetxt('matr.txt', A)
+np.savetxt('rside.txt', B)
+
+A = np.loadtxt('matr.txt')
+B = np.loadtxt('rside.txt')
+
 solution = np.linalg.solve(A, B)
 
-print("Решение системы уравнений:", solution)
+np.savetxt('sol.txt', solution)
+
+print("Решение системы уравнений сохранено в файл sol.txt.: ", solution)
