dc868ae5f8a98a99692ff063557f14273779a5aa
AvardSir <asasiincrit@bk.ru>
2024-08-29 17:10:02 +0300

    алал

--- react teach/js/index.js
+++ react teach/js/index.js
@@ -1,6 +1,5 @@
 
-
-let element=<input placeholder="я текст" onClick={console.log('')}></input>
-
+let element=<input placeholder="я текст" onClick={hellp_print}></input>
 const app=document.getElementById('app')
 ReactDOM.render(element,app)
+//  алал
