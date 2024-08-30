e2324ca80af402460a20a6b6fe30603358c5f2af
AvardSir <asasiincrit@bk.ru>
2024-08-30 10:43:45 +0300

    Inlcuding React in proj  with Node js

--- react teach/js/index.js
+++ react teach/js/index.js
@@ -1,5 +1,12 @@
+const  hellp_print=() => {console.log('hello! oncliock')
+    
+}
 
-let element=<input placeholder="я текст" onClick={hellp_print}></input>
+const  onMouss=() => {console.log('hello! onMouss')
+    
+}
+3+2==2+5?console.log('2+3'):console.log('yo')
+let par='priv how a u'
+let element=<input placeholder={par} onClick={hellp_print} onMouseEnter={onMouss}></input>
 const app=document.getElementById('app')
 ReactDOM.render(element,app)
-//  алал
