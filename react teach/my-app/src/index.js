ed360b713a5834550ab5846e7c9fa5b73f1c7726
AvardSir <asasiincrit@bk.ru>
2024-08-31 18:31:32 +0300

    Немного поработал по изменял

--- react teach/my-app/src/index.js
+++ react teach/my-app/src/index.js
@@ -1,49 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Убедитесь, что импорт правильный
+import App from './app.js';
 
 
-
-class Input {
-  let par = 'priv how a u';
-  render(){
-    return(<input 
-      placeholder = { par } 
-      onClick = { helloPrint } 
-      onMouseEnter = { onMouse }
-      />)
-  }
-
-
-  helloPrint() {
-    return console.log('hello! onClick');
-  }
-
-  onMouse() {
-    return console.log('hello! onMouse');
-  }
-}
-function EmmtyText() {
-  return (<p>Привет это текст текст 2+1</p>)
-}
-class NewText extends React.Component {
-  render() {
-    return (<p>Проверка классов</p>)
-
-  }
-}
-function App() {
-  return (
-    <div className='App'>
-      <Input />
-      <EmmtyText />
-      <EmmtyText />
-      <EmmtyText />
-      <NewText />
-
-    </div>
-
-  );
-}
 
 // Создаем корень с помощью createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
