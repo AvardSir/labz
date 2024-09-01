7e2cff70fcaeeb41a1e9593e75a37ef0e51dd967
AvardSir <asasiincrit@bk.ru>
2024-09-01 21:03:18 +0300

    img work. reWrite SampleText in page. fix promlem with doctytep html(dont need scrtipt in Html in React)

--- react teach/my-app/src/index.js
+++ react teach/my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Убедитесь, что импорт правильный
-import app_ from './img/app.js';
+import App_ from './app.js';
 import  './css/index.css'
 
 
 // console.log(<EmmtyText></EmmtyText>)
 // Создаем корень с помощью createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<app_/>); // Рендерим компонент App
+root.render(<App_/>); // Рендерим компонент App
