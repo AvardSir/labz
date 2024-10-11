a993f7fcef86f5171e07152f62c5661ab1943bd5
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-10-11 22:11:44 +0300

    start express js

--- node js teach/index.js
+++ node js teach/index.js
@@ -1,32 +1,43 @@
-const http = require('http');
-const fs = require('fs');
+// const http=require('http')
+// const fs=require('fs')
+// let server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+//     if (req.url === '/') {
+//         fs.createReadStream('templates/index.html').pipe(res);
+//     } else if (req.url === '/about') {
+//         fs.createReadStream('templates/about.html').pipe(res);
+//     }
+//     else 
+//    {
+//         fs.createReadStream('templates/error.html').pipe(res);
+//     }
+    
+//     // res.end('Hello <b>ddd<b>')
+// })
 
-    if (req.url === '/') {
-        const stream = fs.createReadStream('templates/index.html');
-        stream.pipe(res);
-        stream.on('error', (err) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain;charset=utf-8' });
-            res.end('Ошибка при чтении файла index.html');
-        });
-    } else if (req.url === '/about') {
-        const stream = fs.createReadStream('templates/about.html');
-        stream.pipe(res);
-        stream.on('error', (err) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain;charset=utf-8' });
-            res.end('Ошибка при чтении файла about.html');
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain;charset=utf-8' });
-        res.end('404 Страница не найдена');
-    }
-});
+// const PORT=3000
+// const HOST='localhost'
 
-const PORT = 3000;
-const HOST = 'localhost';
+// server.listen(PORT,HOST,()=>{
+//     console.log(`Сервер запущен ${PORT} и ${HOST}`)
+// }) 
 
-server.listen(PORT, HOST, () => {
-    console.log(`Сервер запущен на ${HOST}:${PORT}`);
-});
+const express=require('express')
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send('This home page')
+})
+
+app.get('/user/:username/:id',(req,res)=>{
+    res.send(`This iser ${req.params.username} id is ${req.params.id}`)
+})
+
+app.get('/about',(req,res)=>{
+    res.send('пронас')
+})
+PORT=3000
+app.listen(PORT,()=>{
+    console.log(`server start localhost:${PORT}`)
+})
