c52c71c5ae536d8396d0c51d436a5b675b01c1ae
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-01-12 12:55:14 +0300

    try codium

--- react teach/mvp teach/my-app/src/models/UserModel.js
+++ react teach/mvp teach/my-app/src/models/UserModel.js
@@ -3,7 +3,14 @@ export class UserModel {
     this.user = { name: "John Doe", age: 30 };
   }
 
+  /**
+   * Gets the current user.
+   * @returns {object} The current user.
+   */
   getUser() {
+    console.log("UserModel: getUser");
+    console.log("UserModel: getUser: user:", this.user);
+    console.log("UserModel: getUser: returning user:", this.user);
     return this.user;
   }
 
