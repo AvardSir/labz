936bc189cc223a6c600d4f1040169255f9dbc335
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-01-16 18:17:03 +0300

    polindrom done, two some, upperBound

--- react teach/mvp teach/my-node-project/index.js
+++ react teach/mvp teach/my-node-project/index.js
@@ -1,17 +1,19 @@
-arr=[1,2,3,4]
-
-target=3
-function upperBound(arr,target) {
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        if (i>target){
-            return i
-        }
-        return arr.length-1
-        
-    }
-    
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+let bad_bracket=[')',']','}']
+let good_bracket=[]
+for (let i = 0; i < bad_bracket.length; i++) {
+    const element = bad_bracket[i];
+    good_bracket[i]=bad_bracket[i].reverse
 }
 
-console.log('2')
-console.log(upperBound(arr,target))
+console.log(good_bracket)
+var isValid = function(s) {
+    for (let i = 0; i < array.length; i++) {
+        const element = s[i];
+        if (element==')'){}
+    }
+};
+
