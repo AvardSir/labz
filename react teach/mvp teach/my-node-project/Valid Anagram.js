ec7af926eeb78ed8e71cce6803308f1cba3caa49
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-02-05 21:34:13 +0300

    Valid anagram

--- react teach/mvp teach/my-node-project/Valid Anagram.js
+++ react teach/mvp teach/my-node-project/Valid Anagram.js
@@ -20,23 +20,48 @@
 //     return true 
 // };
 
+// чужое решение через словарь Map()
+
+// var isAnagram = function(s, t) {
+//     if (s.length !== t.length) {
+//         return false;
+//     }
+
+//     const counter = new Map();
+
+//     for (let char of s) {
+//         counter.set(char, (counter.get(char) || 0) + 1);
+//     }
+
+//     for (let char of t) {
+//         if (!counter.has(char) || counter.get(char) === 0) {
+//             return false;
+//         }
+//         counter.set(char, counter.get(char) - 1);
+//     }
+
+//     return true;    
+// };
 
 var isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
 
-    const counter = new Map();
-
+    const count = Array(26).fill(0);
+// вот тут обяснение обсидиан 
     for (let char of s) {
-        counter.set(char, (counter.get(char) || 0) + 1);
+        count[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
     }
+// выше короч. мы берем мы под индексом для буквы индексом 0 это а делаем +1 для z будет индекс 26
+// короч как словарь но круче тк индексы+не use dict
 
+// ниже проверка, если у iй буквы 0 значит нету в первой строке return false
     for (let char of t) {
-        if (!counter.has(char) || counter.get(char) === 0) {
+        if (count[char.charCodeAt(0) - 'a'.charCodeAt(0)] === 0) {
             return false;
         }
-        counter.set(char, counter.get(char) - 1);
+        count[char.charCodeAt(0) - 'a'.charCodeAt(0)] -= 1;
     }
 
     return true;    
