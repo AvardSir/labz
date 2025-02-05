4b3e18f5986cf5146b4606a153ca1c998238d0e0
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-02-05 23:02:03 +0300

    First Unique Character in a String

--- react teach/mvp teach/my-node-project/Reverse String.js
+++ react teach/mvp teach/my-node-project/Reverse String.js
@@ -1,68 +1,36 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
  */
-// мое решение бьт 7 процентов
-// var isAnagram = function(s, t) {
-//     if (s.length!=t.length) {
-//         return false
-//     }
-//     s= s.split('').sort()
-//     t= t.split('').sort()
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i]!=t[i]){
-//             return false
-//         }
-//         // const element = array[i];
+// мое решение
+// var reverseString = function(s) {
+//     len=s.length
+//     len_mid=Math.floor(len/2)
+//     len--
+//     for (let i = 0; i < len_mid; i++) {
+//         [s[i],s[len-i]]=[s[len-i],s[i]]
+//         // const element = s[i];
+//         // console.log(s[i])
         
 //     }
-//     return true 
+//     return s
 // };
 
-// чужое решение через словарь Map()
+// чужое но время+- тоже хотя памяти использует больше
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let left = 0
+    let right = s.length - 1
 
-// var isAnagram = function(s, t) {
-//     if (s.length !== t.length) {
-//         return false;
-//     }
+    while(left < right){
+        let temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
 
-//     const counter = new Map();
-
-//     for (let char of s) {
-//         counter.set(char, (counter.get(char) || 0) + 1);
-//     }
-
-//     for (let char of t) {
-//         if (!counter.has(char) || counter.get(char) === 0) {
-//             return false;
-//         }
-//         counter.set(char, counter.get(char) - 1);
-//     }
-
-//     return true;    
-// };
-
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
+        left++
+        right--
     }
-
-    const count = Array(26).fill(0);
-// вот тут обяснение обсидиан 
-    for (let char of s) {
-        count[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
-    }
-// выше короч. мы берем мы под индексом для буквы индексом 0 это а делаем +1 для z будет индекс 26
-// короч как словарь но круче тк индексы+не use dict
-
-// ниже проверка, если у iй буквы 0 значит нету в первой строке return false
-    for (let char of t) {
-        if (count[char.charCodeAt(0) - 'a'.charCodeAt(0)] === 0) {
-            return false;
-        }
-        count[char.charCodeAt(0) - 'a'.charCodeAt(0)] -= 1;
-    }
-
-    return true;    
 };
