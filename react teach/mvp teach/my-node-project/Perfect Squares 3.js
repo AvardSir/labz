626e2da8f5b3fc6b2ddfb29f6c3536db0799dc2c
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-02-13 19:04:23 +0300

    rob  can myself  also maxProduct with other solution. and i think i gat how to Perfect Squares

---
+++ react teach/mvp teach/my-node-project/Perfect Squares 3.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+    const dp=[1,2,3,1]
+    // const memo={}
+    // memo[0]=0
+    // memo[1]=1
+    // memo[2]=2
+    // memo[3]=3
+    // memo[4]=1
+
+
+
+let aaaa=Math.floor(Math.sqrt(13))
+
+let n=13
+n=19
+
+n=9*10
+810
+//случай 90, это вроде 10*9 а на деле это 9**2+3**2= 81+9=90
+
+let aaa=numSquares(n) 
+//10
+
+
+let ab=32
+
+
