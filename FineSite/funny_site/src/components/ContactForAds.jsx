b649477eb084f26565857f18d3dc8912afb0cc02
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-04-08 10:03:05 +0300

    ad block for site

---
+++ FineSite/funny_site/src/components/ContactForAds.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/ContactForAds.css";
+
+const ContactForAds = () => {
+  return (
+    <div className="contact-ads-block">
+      <h2>Связаться по поводу рекламы</h2>
+      <p>
+        По вопросам размещения рекламы пишите на:{" "}
+        <a href="mailto:ads_FunnySite@gmail.com">ads_FunnySite@gmail.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default ContactForAds;
--- FineSite/funny_site/src/pages/HomePage.jsx
+++ FineSite/funny_site/src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
-//HomePage.jsx
 import React from "react";
-import { Header } from "../components/Header"; // Используем именованный импорт
-import { Autification } from "../components/Autification"; // Используем именованный импорт
-// import { SearchSection } from "../components/SearchSection"; // Используем именованный импорт
+import { Header } from "../components/Header";
+import { Autification } from "../components/Autification";
+import AdBlock from "../components/AdBlock";
+import ContactForAds from "../components/ContactForAds";
 
-import "../css/HomePage.css"; // Простой импорт стилей
+import "../css/HomePage.css";
+import "../css/AdBlock.css";
+import "../css/ContactForAds.css";
 
 const HomePage = ({ what = 'anecdote' }) => {
   return (
     <div className="main-container">
       <Header />
       <Autification />
-      
+
+      {/* Flex-контейнер для двух блоков */}
+      <div className="ads-row">
+  <AdBlock />
+  <ContactForAds />
+</div>
+
     </div>
   );
 };
